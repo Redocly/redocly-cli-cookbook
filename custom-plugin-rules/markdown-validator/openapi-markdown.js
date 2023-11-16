bfc1692504d45f40dfd1791e3ac93ea97a66c025
Lorna Jane Mitchell <lorna.mitchell@redocly.com>
2023-11-16 17:25:31 +0000

    Markdown validation rule in custom plugin (#9)
    Adding custom plugin example for markdown validation

---
+++ custom-plugin-rules/markdown-validator/openapi-markdown.js
@@ -0,0 +1,10 @@
+const ValidateMarkdown = require("./rule-validate-markdown.js");
+
+module.exports = {
+  id: "openapi-markdown",
+  rules: {
+    oas3: {
+      validate: ValidateMarkdown,
+    },
+  },
+};
---
+++ custom-plugin-rules/markdown-validator/rule-validate-markdown.js
@@ -0,0 +1,78 @@
+const markdownlint = require("markdownlint");
+const config = {
+  // the list is here https://github.com/DavidAnson/markdownlint#rules--aliases
+  MD013: { line_length: 120 },
+  MD041: false, // first line should be h1
+  MD047: false, // should end with newline
+};
+
+function checkString(description, ctx) {
+  let options = {
+    strings: {
+      desc: description,
+    },
+    config: config,
+  };
+  markdownlint(options, function callback(err, result) {
+    if (!err) {
+      // if there's no problem do nothing
+      if (result.desc.length) {
+        // desc is the key in the options.strings object
+        let lines = description.split("\n");
+
+        result.desc.forEach((desc) => {
+          message = desc.ruleDescription;
+          // add line number context for longer entries
+          if (desc.lineNumber > 1) {
+            message =
+              message +
+              " (near: " +
+              lines[desc.lineNumber].substring(0, 20) +
+              "... )";
+          }
+
+          ctx.report({
+            message: message,
+            location: ctx.location.child("description"),
+          });
+        });
+      }
+    }
+  });
+}
+
+function ValidateMarkdown() {
+  console.log("OpenAPI Markdown: validate");
+  return {
+    Info: {
+      enter({ description }, ctx) {
+        if (description) {
+          return checkString(description, ctx);
+        }
+      },
+    },
+    Tag: {
+      enter({ description }, ctx) {
+        if (description) {
+          return checkString(description, ctx);
+        }
+      },
+    },
+    Operation: {
+      enter({ description }, ctx) {
+        if (description) {
+          return checkString(description, ctx);
+        }
+      },
+    },
+    Parameter: {
+      enter({ description }, ctx) {
+        if (description) {
+          return checkString(description, ctx);
+        }
+      },
+    },
+  };
+}
+
+module.exports = ValidateMarkdown;
