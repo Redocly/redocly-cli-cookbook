17dadfb6325fe83bde1aaa07ab853931f50cdce0
Lorna Mitchell <lorna.mitchell@redocly.com>
2024-08-16 16:45:54 +0100

    Add sorting decorators for properties, operatiosns and enums

--- custom-plugin-decorators/sorting/sorting.js
+++ custom-plugin-decorators/sorting/sorting.js
@@ -1,12 +1,21 @@
 const SortTagsAlphabetically = require('./sort-tags');
+const SortOperationsAlphabetically = require('./sort-operations');
+const SortEnumsAlphabetically= require('./sort-enums');
 const SortMethods = require('./sort-methods');
+const SortPropertiesAlphabetically = require('./sort-props-alpha');
+const SortPropertiesRequiredFirst = require('./sort-props-required');
+
 
 module.exports = {
   id: 'sorting',
   decorators: {
     oas3: {
       'tags-alphabetical': SortTagsAlphabetically,
+      'operations-alphabetical': SortOperationsAlphabetically,
+      'enums-alphabetical': SortEnumsAlphabetically,
       'methods': SortMethods,
+      'properties-alphabetical': SortPropertiesAlphabetically,
+      'properties-required-first': SortPropertiesRequiredFirst,
     }
   }
 	
