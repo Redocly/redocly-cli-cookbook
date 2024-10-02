b94c3431951caec9a3a7195ffa6387e9a4008c22
Lrubin <Lrubin@nylas.com>
2024-10-02 13:07:54 -0700

    clean up these

--- custom-plugin-rules/code-sample-checks/x-code-sample-checks.js
+++ custom-plugin-rules/code-sample-checks/x-code-sample-checks.js
@@ -1,14 +1,10 @@
-// const RequireBashExample = require('./rules/require-bash-example.js');
 const CheckSDKCoverage = require('./rules/check-sdk-coverage.js');
-
-
 
 module.exports = function myRulesPlugin() {
   return {
     id: 'x-code-samples-check',
     rules: {
       oas3: {
-        // 'require-bash-sample': RequireBashExample, //court
         'check-sdk-coverage': CheckSDKCoverage
       },
     },
