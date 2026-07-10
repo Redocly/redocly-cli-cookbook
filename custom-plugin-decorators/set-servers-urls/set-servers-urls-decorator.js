0afe16ac2c7aff9af847d65bb62cf1132128d04c
Andrew Tatomyr <andrew.tatomyr@gmail.com>
2026-07-10 13:13:18 +0200

    Apply suggestion from @tatomyr

--- custom-plugin-decorators/set-servers-urls/set-servers-urls-decorator.js
+++ custom-plugin-decorators/set-servers-urls/set-servers-urls-decorator.js
@@ -3,8 +3,6 @@ export default function SetServersUrls({ serverUrl = [] }) {
   return {
     Root: {
       leave(node) {
-        // The serverUrl argument is correct as it is, unfortunately.
-        // Renaming it to serverUrls breaks the plugin functionality.
         const serverUrlsIsAValidArray =
           Array.isArray(serverUrl) && serverUrl.length > 0;
 
